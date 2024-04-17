57cea312d4bf7717ccd4c319d6dbafb14c048765
sandy <sandeshjadhav2002@gmail.com>
2024-04-17 20:59:14 +0530

    add BookCard

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,24 @@
 import Banner from "@/app/(home)/components/Banner";
 import Image from "next/image";
+import BookList from "./components/BookList";
 
-export default function Home() {
+export default async function Home() {
+
+  //data fetching
+
+  // const response = await fetch(`${process.env.BACKEND_URL}/books`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/books`);
+  if(!response.ok){
+    throw new Error('Am Error occured while fetching the books')
+  }
+  const books = await response.json();
+  console.log('books',books);
+  
+
   return (
     <>
     <Banner/>
+    <BookList books={books}/>
     </>
   );
 }
